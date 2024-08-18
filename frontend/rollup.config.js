9f2f42359f130beb7662232aa49dc0c2d20c53bb
Clarise Troncoso <clarise@MacBook-Air-de-Clarise.local>
2024-08-18 20:54:34 -0300

    actualizacion rollup.config.js

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -1,8 +1,11 @@
 import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import css from 'rollup-plugin-css-only';
+import terser from '@rollup/plugin-terser';
+import serve from 'rollup-plugin-serve'; // Importa el plugin serve
 
+const production = !process.env.ROLLUP_WATCH;
 export default {
   input: 'src/main.js',
   output: {
@@ -19,6 +22,14 @@ export default {
     }),
     resolve(),
     commonjs(),
+    css({ 
+      output: 'public/build/styles.css'
+    }),
+    !production && serve({
+      open: true, // Abre el navegador automáticamente
+      contentBase: 'public',
+      port: 3000 // Configura el puerto 
+    }),
     production && terser()
   ]
 };
