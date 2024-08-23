4525f77237e3ab611508bde3f0f3a34f4f6bc29d
CrlsAlejandro97 <aleocampoiza97@gmail.com>
2024-08-23 18:39:48 +0000

    Estructura Crear pool de copra

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,36 +1,16 @@
 <script>
-	export let name;
 	import Footer from "./Components/footer.svelte";
 	import Header from "./Components/header.svelte";
+    import Form from "./Components/Form.svelte";
 </script>
 
 <main>
 	<Header />
-	
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Form />
 	
 	<Footer />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
