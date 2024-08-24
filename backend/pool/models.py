3760750b11617353f17c2d7238712978ebe47fb8
Juan Cruz <juancarrizoast@gmail.com>
2024-08-24 15:57:59 -0300

    pool creation model

--- backend/pool/models.py
+++ backend/pool/models.py
@@ -11,26 +11,36 @@ class Categoria(models.Model):
     
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
-    descripcion = models.TextField()
+    descripcion = models.TextField(blank=True, null=True)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
-    cantidad_disponible = models.IntegerField()
+    cantidad = models.IntegerField()
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nombre
     
 class Pool(models.Model):
+    ESTADO_CHOICES = [
+        ('abierto', 'Abierto'),
+        ('listo', 'Listo'),
+        ('en_curso', 'En Curso'),
+        ('finalizado', 'Finalizado'),
+        ('cancelado', 'Cancelado'),
+    ]
+
     titulo = models.CharField(max_length=100)
-    descripcion = models.TextField()
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
-    cantidad_disponible = models.IntegerField()
-    cantidad_minima_participante = models.IntegerField()
-    #ubicacion_retiro = models.ForeignKey(Ubicacion, on_delete=models.CASCADE)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    descripcion = models.TextField(blank=True, null=True)
+    minimo_participantes = models.IntegerField(default=1)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
     creador = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     fecha_cierre = models.DateTimeField()
+    
+    estado = models.CharField(
+        max_length=20,
+        choices=ESTADO_CHOICES,
+        default='abierto'
+    )
 
     def __str__(self):
         return self.titulo
--- backend/pool/views.py
+++ backend/pool/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+class PoolCreateView(generics.CreateAPIView):
+    queryset = Pool.objects.all()
+    serializer_class = PoolSerializer
+    permission_classes = [IsAuthenticated]  # Requiere autenticación
+
+    def perform_create(self, serializer):
+        # El creador será el usuario autenticado
+        serializer.save(creador=self.request.user)
