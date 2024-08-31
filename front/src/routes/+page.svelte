a112256ac91f8afebaa410cdc98d7ef914613811
Clari <clarise@MacBook-Air-de-Clarise.local>
2024-08-31 18:26:09 -0300

    add logo and header

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,7 +1,24 @@
 <script>
-    import Home from '../lib/componentes/Home.svelte'
+    import Inicio from '../lib/componentes/Home.svelte';
+    import Header from "../lib/componentes/Header.svelte";
+    import Footer from "../lib/componentes/Footer.svelte";
+    import Perfil from "../lib/componentes/perfil.svelte";
+    
+    
 </script>
 
+<Header/> 
 <main>
-    <Home />
+    
+
 </main>
+
+<Footer/>
+
+<style>
+main {
+  margin-top: 80px; /* Ajusta este valor para coincidir con la altura del header */
+  padding: 20px;
+}
+</style>
+
