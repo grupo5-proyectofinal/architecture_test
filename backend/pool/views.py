1679870ae2201c82093969260c5c1afae4acd912
Juan Cruz <juancarrizoast@gmail.com>
2024-09-03 18:38:21 -0300

    poolshop creation and list

--- backend/pool/views.py
+++ backend/pool/views.py
@@ -1,26 +1,29 @@
-from rest_framework import generics
 from .serializers import CreatePoolSerializer, ListPoolSerializer, PoolDetailSerializer
 from .models import Pool
 from django.contrib.auth import get_user_model
+from rest_framework import permissions, generics
 
-User = get_user_model()
 
 
 class PoolListCreateView(generics.ListCreateAPIView):
     queryset = Pool.objects.all()
-
+    permission_classes = [permissions.AllowAny]
+    
     def get_serializer_class(self):
         if self.request.method == 'GET':
             return ListPoolSerializer
         return CreatePoolSerializer
 
     def perform_create(self, serializer):
+        User = get_user_model()
         usuario = User.objects.get(username="juan")
         serializer.save(creador=usuario)  
 
     
+
 class PoolDetailUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Pool.objects.all()
+    permission_classes = [permissions.AllowAny]
 
     def get_serializer_class(self):
         if self.request.method == 'GET':
