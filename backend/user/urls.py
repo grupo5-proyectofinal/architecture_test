9a4d8dd2c0faaae936420bcd44b14fbe10fe5dcb
Juan Cruz <juancarrizoast@gmail.com>
2024-11-16 18:44:00 -0300

    my pools view

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MeView
+from .views import MeView, MyPoolsView
 
 urlpatterns = [
-    path('me/', MeView.as_view(), name='me'),
+    path('', MeView.as_view(), name='me'),
+    path('pools/', MyPoolsView.as_view(), name='my-pools'),
 ]
