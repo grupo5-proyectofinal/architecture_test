b78dd7ae878171a28e695f4e7bb27f2f4b5a8fd4
Juan Cruz <juancarrizoast@gmail.com>
2024-11-26 15:12:15 -0300

    endpoint para crear categorias

--- backend/pool/urls.py
+++ backend/pool/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import PoolDetailUpdateDeleteView, PoolListCreateView, CategoriaListView, JoinPoolView, LeavePoolView
+from .views import PoolDetailUpdateDeleteView, PoolListCreateView, CategoriaListCreateView, JoinPoolView, LeavePoolView
 
 urlpatterns = [
     path('pools/', PoolListCreateView.as_view(), name='pool-list-create'),
     path('pools/<int:pk>/', PoolDetailUpdateDeleteView.as_view(), name='pool-detail'),
     path('pools/<int:pk>/join/', JoinPoolView.as_view(), name='join-pool'),
     path('pools/<int:pk>/leave/', LeavePoolView.as_view(), name='leave-pool'),
-    path('categories/', CategoriaListView.as_view(), name='categories-list'),
+    path('categories/', CategoriaListCreateView.as_view(), name='categories-list'),
 ]
